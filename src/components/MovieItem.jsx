16f0f6b4320d05e30a6d233b6fe6d068b1f8a5ea
veronkrot <veronkrotenko@gmail.com>
2020-04-01 18:55:11 +0300

    added films cards

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 
-const MovieItem = (props) => {
-return (
-    <div>
-        <p>{props.movie.title}</p>
-        <button onClick={props.removeMovie.bind(this, props.movie)}>
-            Delete movie
-        </button>
-    </div>
-)
-
+const MovieItem = props => {
+    const {movie, removeMovie, addMovieToWillWatch} = props;
+    return (
+        <div className="card">
+            <img
+                className="card-img-top"
+                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path ||
+                movie.poster_path}`}
+                alt=""
+            />
+            <div className="card-body">
+                <h6 className="card-title">{movie.title}</h6>
+                <div className="d-flex justify-content-between align-items-center">
+                    <p className="mb-0">Rating: {movie.vote_average}</p>
+                    <button
+                        type="button"
+                        className="btn btn-info"
+                        onClick={addMovieToWillWatch.bind(null, movie)}>
+                        Will Watch
+                    </button>
+                </div>
+                <button onClick={removeMovie.bind(null, movie)}
+                        type="button"
+                        className="btn btn-secondary">
+                    Delete movie
+                </button>
+            </div>
+        </div>
+    )
 };
 
 export default MovieItem;
