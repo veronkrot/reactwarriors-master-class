1f6a85abe59d2232c533f7a290a0b0d519e6c08e
veronkrot <veronkrotenko@gmail.com>
2020-04-08 14:37:22 +0300

    Updates

---
+++ precache-manifest.d1972f8383cd855c55bb5b197b7375df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c34383b7bf6601d1b35d",
+    "url": "/reactwarriors-master-class/static/js/runtime~main.199926b5.js"
+  },
+  {
+    "revision": "9f09c26e34fb721459a1",
+    "url": "/reactwarriors-master-class/static/js/main.b9827ce7.chunk.js"
+  },
+  {
+    "revision": "5c606b019947d783afc6",
+    "url": "/reactwarriors-master-class/static/js/2.1c8d0edb.chunk.js"
+  },
+  {
+    "revision": "9f09c26e34fb721459a1",
+    "url": "/reactwarriors-master-class/static/css/main.29e2e669.chunk.css"
+  },
+  {
+    "revision": "5c606b019947d783afc6",
+    "url": "/reactwarriors-master-class/static/css/2.465da108.chunk.css"
+  },
+  {
+    "revision": "ef0db354e4f882294e11c57e8415f70d",
+    "url": "/reactwarriors-master-class/index.html"
+  }
+];
